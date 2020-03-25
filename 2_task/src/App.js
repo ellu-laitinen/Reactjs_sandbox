29d237082d843dcf3521ccd399532018c07fcf63
ElluLaitinen <ellu.laitinen85@gmail.com>
2020-03-25 13:22:49 +0200

    add card

--- 2_task/src/App.js
+++ 2_task/src/App.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import './App.css';
+import Person from './Person/Person';
 
 const App = () => {
   return (
   <div>
     <h1>Hello, World!</h1>
     <p>Hello, world</p>
+    <Person />
   </div>
   );
 };
 
 export default App;
+
---
+++ 2_task/src/Person/Person.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Person.css';
+
+const Person = () => {
+    return ( <div className="card">
+        <p>Hello, my name is Ellu and I am 34 years old.</p></div>
+    );
+};
+
+export default Person;
