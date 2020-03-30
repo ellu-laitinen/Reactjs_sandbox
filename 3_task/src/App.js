c5a5e0bdd1055c66a97de521a97b4fdee3fddd6e
ElluLaitinen <ellu.laitinen85@gmail.com>
2020-03-30 11:29:44 +0300

    add buttons

--- 3_task/src/App.js
+++ 3_task/src/App.js
@@ -4,14 +4,31 @@ import './App.css';
 class App extends Component {
 
   state = {
-    likes: 100
-  }
+    likes: 0
+  };
+
+  addLikes = () => {
+    this.setState(
+      {likes:this.state.likes + 1})};
+
+  removeLikes = () => {
+    this.setState(
+      {likes:this.state.likes -1})};
+
+  resetLikes = () => {
+    this.setState(
+      {likes:this.state.likes =0})};
+
+
   render () {
     return (
       <div>
       <h1>Total likes:{this.state.likes}</h1>
+      <button onClick={this.addLikes}>Add likes</button>
+      <button onClick={this.removeLikes}>Remove likes</button>
+      <button onClick ={this.resetLikes}>Reset likes</button>
       </div>
-    )
+    );
   }
 }
 
