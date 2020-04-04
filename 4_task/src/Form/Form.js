91f8315d2c2c2357a9d37beb081c7399a38c5c6c
ElluLaitinen <ellu.laitinen85@gmail.com>
2020-04-04 11:51:58 +0300

    add stying

--- 4_task/src/Form/Form.js
+++ 4_task/src/Form/Form.js
@@ -6,18 +6,23 @@ const Form = ({changeHandler}) => {
     return (
         <div className="form">
         <form>
-        <label htmlFor="firstname">First name: </label>
-          <input type="text" id="firstname" name="firstname" onChange={changeHandler}/>
+          
+        <label htmlFor="firstname">First name:</label>
+          <input type="text" id="firstname" name="firstname" onChange={changeHandler}/> 
           <br/>
-          <label htmlFor="lastname">Last name: </label>
-          <input type="text" id="lastname" name="lastname" onChange={changeHandler}/>
+          
+          <label htmlFor="lastname">Last name:</label>
+          <input type="text" id="lastname" name="lastname" onChange={changeHandler}/> 
           <br/>
+          
           <label htmlFor="phone">Phone number: </label>
           <input type="text" id="phone" name="phone" onChange={changeHandler}/>
           <br/>
+         
           <label htmlFor="message">Message: </label>
           <textarea type="textarea" id="message" name="message" onChange={changeHandler} />     
-        </form>    
+      
+       </form>    
   </div>
     );
 }
