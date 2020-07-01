fc710cbe8766233f343f111f493f5d63fb164111
ElluLaitinen <ellu.laitinen85@gmail.com>
2020-07-01 17:43:05 +0300

    add comic

---
+++ final-project/src/Components/Comic/Comic.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import './Comic.css'
+
+
+const Comic = () => {
+    return (
+        <div className="comicarea">
+            <h1 className="comictitle">A day in the life of the Green Ninja Coder</h1>
+
+            <img className="comic" src="images/1.jpg" alt="" />
+            <img className="comic" src="images/2.jpg" alt="" />
+            <img className="comic" src="images/3.jpg" alt="" />
+            <img className="comic" src="images/4.jpg" alt="" />
+            <img className="comic error2" src="images/5.jpg" alt="" />
+            <img className="comic" src="images/6.jpg" alt="" />
+            <img className="comic" src="images/7.jpg" alt="" />
+            <img className="comic" src="images/8.jpg" alt="" />
+            <img className="comic" src="images/9.jpg" alt="" />
+            <img className="comic" src="images/10.jpg" alt="" />
+            <img className="comic" src="images/11.jpg" alt="" />
+            <img className="comic" src="images/12.jpg" alt="" />
+            <img className="comic" src="images/13.jpg" alt="" />
+            <img className="comic" src="images/14.jpg" alt="" />
+            <img className="comic" src="images/15.jpg" alt="" />
+        </div>
+    );
+}
+
+export default Comic;
